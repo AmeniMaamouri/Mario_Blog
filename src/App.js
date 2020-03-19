2d88e7b1b3d7260a37dbcd0e37dca16c23ce8e81
AmeniMaamouri <amanimaamouri@gmail.com>
2020-03-19 15:43:28 +0100

    structure files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {BrowserRouter, Switch} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-      <p>HEllo</p>
-    </div>
+    <BrowserRouter>
+    
+    
+    
+    
+    </BrowserRouter>
   );
 }
 
