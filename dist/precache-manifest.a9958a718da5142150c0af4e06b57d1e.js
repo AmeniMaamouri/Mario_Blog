431ac5f72fa22ac285f3b1c0ffb3f9e1e74adc75
AmeniMaamouri <amanimaamouri@gmail.com>
2020-03-25 12:06:49 +0100

    deploying application

---
+++ dist/precache-manifest.a9958a718da5142150c0af4e06b57d1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfb44e7aae93b0d4e93e903a8c72a97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e3bbc2ac53559ad80cd",
+    "url": "/static/css/main.50f9ef78.chunk.css"
+  },
+  {
+    "revision": "4ae350446e116540e13d",
+    "url": "/static/js/2.211bb0e0.chunk.js"
+  },
+  {
+    "revision": "6b9f671524e4817f679e05b83b5beb67",
+    "url": "/static/js/2.211bb0e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3bbc2ac53559ad80cd",
+    "url": "/static/js/main.ba0ea28e.chunk.js"
+  },
+  {
+    "revision": "5ae5cdff232dc0214069",
+    "url": "/static/js/runtime-main.a8803374.js"
+  }
+]);
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -8,7 +8,7 @@ const Notifications = ({notifications}) => {
 
             <div className='card z-depth-0'>
                 <div className='card-content'>
-                    <span className="card-title">Notifications</span>
+                    <span className="card-title text-bold">Notifications</span>
                     <ul className="notifications">
                        {notifications && notifications.map(notification => {
                            return (
